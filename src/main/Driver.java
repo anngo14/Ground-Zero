70f9d332e887a1092b6d8afb8e128f334d0ba971
anngo14 <31315148+anngo14@users.noreply.github.com>
2019-07-18 00:12:44 -0500

    initial commit

---
+++ src/main/Driver.java
@@ -0,0 +1,17 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Driver extends Application{
+
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+	@Override
+	public void start(Stage arg0) throws Exception {
+		
+	}
+
+}
---
+++ src/model/Goal.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Goal {
+
+	private String name;
+	private String description;
+	private String imgSrc;
+	private int status;
+	private int count;
+	
+	public Goal()
+	{
+		
+	}
+}
