4c90f40a3e750031b7ae336a4e2fea509710e072
anngo14 <31315148+anngo14@users.noreply.github.com>
2019-07-18 16:20:59 -0500

    Goal Model

--- src/model/Goal.java
+++ src/model/Goal.java
@@ -1,15 +1,76 @@
 package model;
 
+import java.util.Date;
+
 public class Goal {
 
 	private String name;
 	private String description;
 	private String imgSrc;
+	private int goal;
 	private int status;
 	private int count;
+	private Date start;
+	private Date end;
 	
 	public Goal()
 	{
-		
+		name = "";
+		description = "";
+		imgSrc = "";
+		goal = 0;
+		status = 0;
+		count = 0;
+		start = new Date();
+		end = new Date();
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getImgSrc() {
+		return imgSrc;
+	}
+	public void setImgSrc(String imgSrc) {
+		this.imgSrc = imgSrc;
+	}
+	public int getGoal() {
+		return goal;
+	}
+	public void setGoal(int goal) {
+		this.goal = goal;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public Date getStart() {
+		return start;
+	}
+	public void setStart(Date start) {
+		this.start = start;
+	}
+	public Date getEnd() {
+		return end;
+	}
+	public void setEnd(Date end) {
+		this.end = end;
 	}
 }
